71933d68a7bea89b0657f4461128e03e17f4102d
AleckDevv <alexsoareslopes688@gmail.com>
2024-08-07 18:45:50 -0300

    feito o código js para ficar alternando entre os sliders a cada 2s

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,16 @@
-let btnMenu = document.querySelector('.aberto');
-btnMenu.addEventListener('click', () => {
-    console.log(btnMenu.classList)
-}); 
+// Animação para o slider ficar trocando sozinho de tempos em tempo
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const inputs = document.querySelectorAll('.slider-input');
+    let currentIndex = 0;
+
+    function switchSlider() {
+        inputs[currentIndex].checked = false;
+        currentIndex = (currentIndex + 1) % inputs.length;
+        inputs[currentIndex].checked = true;
+    }
+
+    setInterval(switchSlider, 2000); // Troca a cada 2 segundos
+});
 
